0bca9a68f9bab629b30237f958f019f856582200
Ethan Ford <ethan.j.ford@hotmail.com>
2025-09-09 22:43:49 -0400

    started on implementation of local model, not completed or tested

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import gradio as gr
 from huggingface_hub import InferenceClient
 
+pipe = None
+stop_infrence = False
 
 client = InferenceClient(
     provider="hf-inference",
@@ -14,27 +16,46 @@ def summarize_text(text):
     """
     if text is None:
         return "No text inserted"
+
+    if use_local_model:
+        try:
+            print("[MODE] local")
+            from transformers import pipeline
+            import torch
+            if pipe is None:
+                pipe = pipeline("summarization" , model="Falconsai/medical_summarization")
+
+            output = pipe(
+                text,
+                return_text=True
+            )
+
+        except Exception as e:
+            return f"Error: {str(e)}"
     
-    try:
-        # Use the summarization model
-        output = client.summarization(text, model="Falconsai/medical_summarization")
-        
-        # Find the summary text
-        if output:
+    else:
+        try:
+            # Use the summarization model
+            output = client.summarization(text, model="Falconsai/medical_summarization")
             
-            return output.summary_text
-        else:
-            return "No summary result"
-            
-    except Exception as e:
-        return f"Error: {str(e)}"
+            # Find the summary text
+            if output:
+                return output.summary_text
+            else:
+                return "No summary result"
+                
+        except Exception as e:
+            return f"Error: {str(e)}"
 
 # Create the Gradio interface
 demo = gr.Interface(
-    fn=summarize_text,
-    inputs=gr.Textbox(type="text"),
-    outputs="text",
-    title="Text Summarization (Action Works!)",
+    fn = summarize_text,
+    inputs = gr.Textbox(type="text"),
+    additional_inputs = [
+        gr.Checkbox(label="Use Local Model", value = False)
+    ],
+    outputs = "text",
+    title="Medical Text Summarization",
     description="Insert a text to summarize"
 )
 
